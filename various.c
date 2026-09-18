9db3d7b32ef0802f2c3c69ab16234c4531047880
HabiburRahmanZihad <e241024@ugrad.iiuc.ac.bd>
2026-09-19 01:01:24 +0600

    Add various.c for formatted input and output examples
    - Implemented a simple program to read an integer and print it with leading zeros.
    - Included commented-out sections for additional input/output examples and alternative data types.

---
+++ various.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main() {
+
+  int a;
+  scanf("%d", &a);
+  printf("%05d", a);
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main() {
+//   //   int a;
+//   //   int b;
+//   //   int c;
+
+//   int a, b, c;
+
+//   //   scanf("%d", &a);
+//   //   scanf("%d", &b);
+//   //   scanf("%d", &c);
+
+//   scanf("%d %d %d", &a, &b, &c);
+
+//   //   printf("%d", a);
+//   //   printf("%d", b);
+//   //   printf("%d", c);
+
+//   printf("%d %d %d", a, b, c);
+
+//   float a;
+//   double a;
+//   scanf("%lf", &a);
+//   printf("%lf", a);
+
+//   return 0;
+// }
